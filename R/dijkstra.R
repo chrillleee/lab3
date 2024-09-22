dce54c75e2926b96c7edcb860b7a5782f9ba774d
Trace <qincu578@student.liu.se>
2024-09-22 22:15:30 +0200

    make the functions ananymous

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -19,6 +19,61 @@
 
 dijkstra <-
 function(graph, init_node){
+    getPriorityQueue <- function(dataFrame,initNode){
+    vertices <- unique(c(dataFrame$v1, dataFrame$v2))
+
+    # Throw error if initNode not in provided data
+    stopifnot(initNode %in% vertices)
+
+    priorityQueue <- lapply(vertices, function(x) createProperties())
+
+    # Have to apply the names explicitly in case of scrambled vertice numbers 
+    names(priorityQueue) <- vertices
+    
+    priorityQueue[[initNode]]$dist <- 0
+    return(priorityQueue)
+    }
+
+    createProperties <- function() {
+    list(dist = Inf, prev = NA)
+    }
+
+    getMinDistVertice <-function(priorityQueue){
+        minVertice <- list()
+        verticeName <- c()
+
+        for(name in names(priorityQueue)){
+            evaluatedVertice <- priorityQueue[[name]]
+
+            if(length(minVertice)==0){
+                minVertice = evaluatedVertice
+                verticeName <- name
+            }
+
+            if(minVertice$dist > evaluatedVertice$dist){
+                minVertice <- evaluatedVertice
+                verticeName <- name
+            }
+        }
+        return(verticeName)
+    }
+
+    getNeighbours <-function(graph, priorityQueue, currentNode){
+        allNodesConnectedToCurrentNode <- graph[graph$v1 == currentNode,]$v2
+        allNodesInPriorityQueue <- as.numeric(names(priorityQueue))
+        # take the intersection of the two vectors to get the nodes still in the queue and is connected
+        # return as a string since we will filter on that later
+        return(as.character(intersect(allNodesConnectedToCurrentNode, allNodesInPriorityQueue)))
+    }
+
+    getWeightFromName <-function(current,neighbor,graph){
+        sort1 <- graph[graph$v1 == current,,]
+        sort2 <- sort1[sort1$v2 == neighbor,]
+        return(sort2$w)
+    }
+
+
+
     expectedNames <- c("v1", "v2", "w")
     stopifnot(identical(names(graph), expectedNames))
     stopifnot(sapply(expectedNames,function(name) is.vector(graph[[name]])))
@@ -49,55 +104,3 @@ function(graph, init_node){
 }
 
 
-getPriorityQueue <- function(dataFrame,initNode){
-    vertices <- unique(c(dataFrame$v1, dataFrame$v2))
-
-    # Throw error if initNode not in provided data
-    stopifnot(initNode %in% vertices)
-
-    priorityQueue <- lapply(vertices, function(x) createProperties())
-
-    # Have to apply the names explicitly in case of scrambled vertice numbers 
-    names(priorityQueue) <- vertices
-    
-    priorityQueue[[initNode]]$dist <- 0
-    return(priorityQueue)
-}
-
-createProperties <- function() {
-  list(dist = Inf, prev = NA)
-}
-
-getMinDistVertice <-function(priorityQueue){
-    minVertice <- list()
-    verticeName <- c()
-
-    for(name in names(priorityQueue)){
-        evaluatedVertice <- priorityQueue[[name]]
-
-        if(length(minVertice)==0){
-            minVertice = evaluatedVertice
-            verticeName <- name
-        }
-
-        if(minVertice$dist > evaluatedVertice$dist){
-            minVertice <- evaluatedVertice
-            verticeName <- name
-        }
-    }
-    return(verticeName)
-}
-
-getNeighbours <-function(graph, priorityQueue, currentNode){
-    allNodesConnectedToCurrentNode <- graph[graph$v1 == currentNode,]$v2
-    allNodesInPriorityQueue <- as.numeric(names(priorityQueue))
-    # take the intersection of the two vectors to get the nodes still in the queue and is connected
-    # return as a string since we will filter on that later
-    return(as.character(intersect(allNodesConnectedToCurrentNode, allNodesInPriorityQueue)))
-}
-
-getWeightFromName <-function(current,neighbor,graph){
-    sort1 <- graph[graph$v1 == current,,]
-    sort2 <- sort1[sort1$v2 == neighbor,]
-    return(sort2$w)
-}
